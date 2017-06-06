93325a25473e2cb5a7e5d3eab93ced2d55799e72
houjingjing <1214992694@qq.com>
2017-06-06 17:54:02 +0800

    h5 摄像头

---
+++ typescript/tutorial/code/01-getter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return 'hello,' + person;
+}
+var user = [0, 1];
+document.body.innerHTML = greeter(user);
---
+++ typescript/tutorial/code/01-getter.ts
@@ -0,0 +1,5 @@
+function greeter(person: string) {
+   return 'hello,' + person;
+}
+var user = [0,1];
+document.body.innerHTML = greeter(user);
